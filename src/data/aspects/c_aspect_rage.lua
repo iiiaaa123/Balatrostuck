503d6042b9ef13eed91be490f0aa39e75a115b14
a-memer <110717705+a-memer@users.noreply.github.com>
2025-02-02 04:54:37 -0600

    Couldn't think of a funny name for this commit but this one adds rage

--- src/data/aspects/c_aspect_rage.lua
+++ src/data/aspects/c_aspect_rage.lua
@@ -29,6 +29,35 @@ function Balatrostuck.INIT.Aspects.c_aspect_rage()
                     (G.GAME.BALATROSTUCK.aspect_levels[self.name] or 0)*0.25
                 }
             }
+        end,
+        use = function(self, context)
+            self:switch_slab()
+        end,
+        can_use = function(self)
+            return true
         end
     }
+
+
+    Balatrostuck.Slab{
+        key = 'rage',
+        atlas = 'HomestuckAspectSlabs',
+        pos = {
+            x = 3,
+            y = 0
+        },
+        config = {},
+        name = 'Aspect of Rage',
+        apply = function(self,slab,context)
+            if context.individual and context.cardarea == G.play then
+                if math.abs(G.GAME.current_round.hands_left - G.GAME.current_round.discards_left) ~= 0 then
+                    return {
+                        x_mult = (slab:level() / 2) * math.abs(G.GAME.current_round.hands_left - G.GAME.current_round.discards_left),
+                        card = context.other_card
+                    }
+                end
+            end
+        end
+    }
+
 end
