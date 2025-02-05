3da1e1cf6d1ea8f6c74b7d869c8f5c4cd9012834
a-memer <110717705+a-memer@users.noreply.github.com>
2025-02-05 03:22:18 -0600

    Parcel Mistress brings you your mail! ...It's just her

--- src/data/jokers/j_parcelmistress.lua
+++ src/data/jokers/j_parcelmistress.lua
@@ -3,17 +3,20 @@ function Balatrostuck.INIT.Jokers.j_parcelmistress()
         name = "Parcel Mistress",
         key = "parcelmistress",
         config = {
-            extra = {
-            }
+            extra = {mult = 7, seal_count = 0}
         },
         loc_txt = {
             ['name'] = 'Parcel Mistress',
             ['text'] = {
-                [1] = "+7 mult for each",
+                [1] = "{C:mult}+#1#{} mult for each",
                 [2] = "card with a seal",
-                [3] = "in your full deck"
+                [3] = "in your full deck",
+                [4] = "{C:inactive}(Currently {C:mult}+#2#{C:inactive} Mult)"
             }
         },
+        loc_vars = function(self,info_queue,card)
+            return {vars = {card.ability.extra.mult,card.ability.extra.mult * card.ability.extra.seal_count}}
+        end,
         pos = {
             x = 2,
             y = 0
@@ -24,6 +27,21 @@ function Balatrostuck.INIT.Jokers.j_parcelmistress()
         eternal_compat = true,
         unlocked = true,
         discovered = true,
-        atlas = 'HomestuckJokers'
+        atlas = 'HomestuckJokers',
+        calculate = function(self,card,context)
+            card.ability.extra.seal_count = 0
+            for _, v in ipairs(G.playing_cards) do
+                if v:get_seal(true) then
+                    card.ability.extra.seal_count = card.ability.extra.seal_count + 1
+                end
+            end
+
+            if context.joker_main then
+                return {
+                    mult = card.ability.extra.seal_count * card.ability.extra.mult,
+                    card = card
+                }
+            end
+        end
     }
 end 
