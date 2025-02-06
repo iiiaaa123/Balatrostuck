bce00799a7299cc88e26efdccbd9735defd022af
a-memer <110717705+a-memer@users.noreply.github.com>
2025-02-06 04:52:43 -0600

    MS Paint + Context.changing_suit

--- src/data/jokers/j_misspaint.lua
+++ src/data/jokers/j_misspaint.lua
@@ -3,16 +3,18 @@ function Balatrostuck.INIT.Jokers.j_misspaint()
         name = "Miss Paint",
         key = "misspaint",
         config = {
-            extra = {
-            }
+            extra = {mult = 1, mult_gain = 0}
         },
         loc_txt = {
             ['name'] = 'Miss Paint',
             ['text'] = {
-                [1] = "Gains +1 mult when a card changes suits",
-                [2] = "[Currently: +4 mult]"
+                [1] = "Gains {C:mult}+#1#{} Mult when a card changes suits",
+                [2] = "(Currently {C:mult}+#2#{C:inactive} Mult)"
             }
         },
+        loc_vars = function(self,info_queue,card)
+            return {vars = {card.ability.extra.mult, card.ability.extra.mult_gain}}
+        end,
         pos = {
             x = 2,
             y = 0
@@ -23,6 +25,22 @@ function Balatrostuck.INIT.Jokers.j_misspaint()
         eternal_compat = true,
         unlocked = true,
         discovered = true,
-        atlas = 'HomestuckJokers'
+        atlas = 'HomestuckJokers',
+        calculate = function(self,card,context)
+            if context.changing_suit then
+                card.ability.extra.mult_gain = card.ability.extra.mult_gain + card.ability.extra.mult
+                return {
+                    message = 'Upgraded!',
+                    card = card
+                }
+            end
+
+            if card.ability.extra.mult_gain > 0 and context.joker_main then
+                return {
+                    mult = card.ability.extra.mult_gain,
+                    card = card
+                }
+            end
+        end
     }
 end 
