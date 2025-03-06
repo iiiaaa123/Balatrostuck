c208840814b4f84b4fde3f5f580b1ed4694b85f8
a-memer <110717705+a-memer@users.noreply.github.com>
2025-03-06 02:38:38 -0600

    Ectoed my Biology

--- data/jokers/j_ectobiology.lua
+++ data/jokers/j_ectobiology.lua
@@ -10,7 +10,7 @@ function Balatrostuck.INIT.Jokers.j_ectobiology()
             ['name'] = 'Ectobiology',
             ['text'] = {
                 [1] = "When {C:attention}Blind{} is selected, create a",
-                [2] = "{C:green}Paradox{} copy of {C:attention}Joker{} to the right"
+                [2] = "{C:paradox}Paradox{} copy of {C:attention}Joker{} to the right"
             }
         },
         pos = {
@@ -23,6 +23,34 @@ function Balatrostuck.INIT.Jokers.j_ectobiology()
         eternal_compat = true,
         unlocked = true,
         discovered = true,
-        atlas = 'HomestuckJokers'
+        atlas = 'HomestuckJokers',
+        calculate = function(self,card,context)
+            if context.setting_blind and context.cardarea == G.jokers then
+                local other_joker = nil
+                for i = 1, #G.jokers.cards - 1 do
+                    if G.jokers.cards[i] == card then 
+                        other_joker = G.jokers.cards[i+1]
+                        break
+                    end
+                end
+            
+            
+                if other_joker then
+                    G.E_MANAGER:add_event(Event({
+                        trigger = 'before',
+                        delay = 0.0,
+                        func = function()
+                            local _card = copy_card(other_joker, nil, nil, nil, card.edition)
+                            _card:start_materialize()
+                            _card:set_edition('e_bstuck_paradox')
+                            _card:add_to_deck()
+                            G.jokers:emplace(_card)
+                            G.GAME.joker_buffer = 0
+                        return true
+                        end
+                    }))
+                end
+            end
+        end
     }
 end 
