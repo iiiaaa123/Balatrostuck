1e39cbbacde4aec6ad08ab5c9091f856a5977236
Gainumki <gabrytechgamer@gmail.com>
2025-04-25 19:01:30 +0200

    Enter the Medium

--- data/jokers/j_enterthemedium.lua
+++ data/jokers/j_enterthemedium.lua
@@ -25,35 +25,37 @@ function Balatrostuck.INIT.Jokers.j_enterthemedium()
         discovered = true,
         atlas = 'HomestuckJokers',
         calculate = function(self,card,context)
-            if context.end_of_round and G.GAME.blind.boss then
+            if context.end_of_round and G.GAME.blind.boss and context.main_eval then
                 local pool = get_current_pool('Joker')
-                local _key = nil
-                
+                local _key = "j_joker"
+                local bstuck_pool = {}
 
-                for i=1, #pool do
-                    sendInfoMessage(pool[i])
-                    if string.find(pool[i],'bstuck') then
-                        local _key = pseudorandom_element(pool,pseudoseed('medium'))
-                        break
+                for k,v in pairs(pool) do
+                    if string.sub(v, 1, 8) == 'j_bstuck' then
+                        bstuck_pool[#bstuck_pool+1] = v
                     end
                 end
+
+                _key = pseudorandom_element(bstuck_pool,pseudoseed('medium'))
 
                 G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.15,func = function() card:flip();play_sound('card1', percent);card:juice_up(0.3, 0.3);return true end }))
                 G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.1,func = function()
                     card:remove_from_deck()
                     card.config.center = G.P_CENTERS[_key]
-                    card:set_ability(card.config.center,true)
+                    card:set_ability(card.config.center.key,true)
                     card:add_to_deck()
                     return true 
                 end}))
                 G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.15,func = function() card:flip();play_sound('tarot2', percent);card:juice_up(0.3, 0.3);return true end }))
             end
         end,
+
         add_to_deck = function(self,card,from_debuff)
             if G.GAME.pool_flags.bstuck_actprogress <= 2 then
                 G.GAME.pool_flags.bstuck_actprogress = 3
             end
         end,
+
         in_pool = function(self,args)
             return G.GAME.pool_flags.bstuck_actprogress >= 2
         end
