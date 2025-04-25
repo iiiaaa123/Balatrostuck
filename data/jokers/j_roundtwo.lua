cc319c333d9f17171a705c6916bfb5aa78ee5e17
CampfireCollective <122759859+GuilloryCraft@users.noreply.github.com>
2025-04-24 18:30:14 -0600

    "reworked" UU slightly idk why it was this

--- data/jokers/j_roundtwo.lua
+++ data/jokers/j_roundtwo.lua
@@ -1,14 +1,16 @@
 function Balatrostuck.INIT.Jokers.j_roundtwo() 
     SMODS.Joker{
-        name = "Round 2",
+        name = "Umbral Ultimatum",
         key = "roundtwo",
         config = {
-            extra = { ante_scaling = 2
+            extra = { 
+                ante_scaling = 2
             }
         },
         loc_txt = {
-            ['name'] = 'Round 2',
+            ['name'] = 'Umbral Ultimatum',
             ['text'] = {
+                'bluh bluh X2 ante scaling + a rare joker after each boss'
             }
         },
         pos = {
@@ -29,25 +31,27 @@ function Balatrostuck.INIT.Jokers.j_roundtwo()
             G.GAME.starting_params.ante_scaling = G.GAME.starting_params.ante_scaling / card.ability.extra.ante_scaling
         end,
         calculate = function(self,card,context)
-            if context.end_of_round and context.cardarea == G.jokers and G.jokers.config.card_limit - (#G.jokers.cards + G.GAME.joker_buffer) > 0 then
-                G.E_MANAGER:add_event(Event({
-                    func = function()
-                        local _card = SMODS.add_card({set = 'Joker', rarity = 3})
-                        _card:start_materialize()
-                        return true
-                end}))
-                card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = localize('k_plus_joker'), colour = G.C.RED})
-            end 
+            if G.GAME.blind.boss and context.end_of_round and context.cardarea == G.jokers then
+                if G.jokers.config.card_limit - (#G.jokers.cards + G.GAME.joker_buffer) > 0 then
+                    G.E_MANAGER:add_event(Event({
+                        func = function()
+                            local _card = SMODS.add_card({set = 'Joker', rarity = 3})
+                            _card:start_materialize()
+                            return true
+                    end}))
+                    card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = localize('k_plus_joker'), colour = G.C.RED})
+                end
+            
+                if not context.blueprint then
+                    G.GAME.starting_params.ante_scaling = G.GAME.starting_params.ante_scaling / card.ability.extra.ante_scaling
+                    card.ability.extra.ante_scaling = card.ability.extra.ante_scaling * 2
+                    G.GAME.starting_params.ante_scaling = G.GAME.starting_params.ante_scaling * card.ability.extra.ante_scaling
 
-            if context.ending_shop then
-                G.GAME.starting_params.ante_scaling = G.GAME.starting_params.ante_scaling / card.ability.extra.ante_scaling
-                card.ability.extra.ante_scaling = card.ability.extra.ante_scaling + 2
-                G.GAME.starting_params.ante_scaling = G.GAME.starting_params.ante_scaling * card.ability.extra.ante_scaling
-
-                return {
-                    message = 'X' .. card.ability.extra.ante_scaling .. ' Blind Requirement!',
-                    card = card
-                }
+                    return {
+                        message = 'X' .. card.ability.extra.ante_scaling .. ' Blind Requirement!',
+                        card = card
+                    }
+                end
             end
         end
 
