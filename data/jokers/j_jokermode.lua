9d0dfde7f7dee049944691a678341b8df5271256
Gainumki <gabrytechgamer@gmail.com>
2025-04-28 00:10:57 +0200

    JOKER MODE

--- data/jokers/j_jokermode.lua
+++ data/jokers/j_jokermode.lua
@@ -5,6 +5,8 @@ function Balatrostuck.INIT.Jokers.j_jokermode()
         key = "jokermode",
         config = {
             extra = {
+                chips = 0,
+                chip_gain = 25
             }
         },
         loc_txt = {
@@ -24,7 +26,26 @@ function Balatrostuck.INIT.Jokers.j_jokermode()
         discovered = true,
         atlas = 'HomestuckJokers',
 
-        calculate = function (self, context)
+        in_pool = function(self)
+            return false
+        end,
+
+        calculate = function (self, card, context)
+            if context.joker_main then
+                return {
+                  chip_mod = card.ability.extra.chips,
+                  message = localize { type = 'variable', key = 'a_chips', vars = { card.ability.extra.chips } }
+                }
+            end
+          
+            if context.before and context.scoring_name == "Flush" and not context.blueprint then
+                card.ability.extra.chips = card.ability.extra.chips + card.ability.extra.chip_gain
+                return {
+                  message = 'UPGRADED!',
+                  colour = G.C.CHIPS,
+                  card = card
+                }
+            end          
         end
     }
 end
