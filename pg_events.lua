922579078e368cadc97c1fda271e1e8ae2d165c2
akaigaming <vriskatfs@proton.me>
2025-05-20 22:48:30 +0100

    Update pg_events.lua

--- pg_events.lua
+++ pg_events.lua
@@ -7,39 +7,39 @@ PG.Vriska = {}
 PG.Caliborn = {}
 
 PG.John.shop_events = {
-    {
+    free_voucher = {
         quip = 'john_free_voucher',
         func = function(_self,card,context)
           
         end
     },
-    {
+    discount_card = {
         quip = 'john_discount_card',
-        func = function (_self,card_context)
+        func = function (_self,card,context)
             
         end
     },
-    {
+    add_pack = {
         quip = 'john_add_pack',
-        func = function (_self,card_context)
+        func = function (_self,card,context)
             
         end
     },
-    {
+    free_reroll = {
         quip = 'john_free_reroll',
-        func = function (_self,card_context)
+        func = function (_self,card,context)
             
         end
     },
-    {
+    add_rare = {
         quip = 'john_add_rare',
-        func = function (_self,card_context)
+        func = function (_self,card,context)
             
         end
     },
-    {
+    add_uncommon = {
         quip = 'john_add_uncommon',
-        func = function (_self,card_context)
+        func = function (_self,card,context)
             
         end
     },
