ba21a9ea4a8e15c227480daea35d321d06574430
CampfireCollective <122759859+GuilloryCraft@users.noreply.github.com>
2025-09-21 17:45:02 -0700

    vriska quips

--- localization/default.lua
+++ localization/default.lua
@@ -112,8 +112,8 @@ return {
                 text = {
                     '{C:dark_edition}Quest #1#:',
                     'Create #2# Consumable',
-                    'Cards in one round',
-                    '(Currently #3#/#2#)'
+                    'Cards in one Blind',
+                    '{C:inactive}(Currently {C:attention}#3#{C:inactive}/#2#)'
                 }
             },
             j_bstuck_backseater_7 ={
@@ -121,15 +121,25 @@ return {
                 text = {
                     '{C:dark_edition}Quest #1#:',
                     'Trigger played cards',
-                    '#2# times in one hand'
+                    '#2# times in one hand,',
+                    'then beat the Blind',
+                    '{C:inactive}(Currently {C:attention}#3#{C:inactive}/#2#)'
                 }
             },
             j_bstuck_backseater_8 ={
                 name = '8acks8er',
                 text = {
                     '{C:dark_edition}Quest #1#:',
-                    'Score #2# Chips',
-                    'in one hand'
+                    'Score #2# Chips in one',
+                    'hand, then beat the Blind'
+                }
+            },
+            j_bstuck_backseater_0 ={
+                name = '8acks8er',
+                text = {
+                    '8ruh my quests 8roke',
+                    'cant have shit in',
+                    '{C:paradox}8alatrostuck'
                 }
             },
         },
@@ -296,7 +306,96 @@ return {
             john_cost={
                 "{C:blue}JOHN: do you want to see my balls",
             },
-
+            backseater_quest_1_start={
+                "AG: Ughhhhhhhh, I've seen gru8s play",
+                "8etter than you! Come oooooooon,",
+                "give me a Str8 Flush at least!",
+            },
+            backseater_quest_1_done={
+                "AG: Okay, may8e you're not completely",
+                "useless after all! You can play",
+                "a good hand if you want to."
+            },
+            backseater_quest_2_start={
+                "AG: Real gamers cut to the chase.",
+                "They power through all the nonsense",
+                "and go for the gold. They cheat.",
+                "It is time you learned to start cheating."
+            },
+            backseater_quest_2_done={
+                "AG: Okay"
+            },
+            backseater_quest_3_start={
+                "AG: I think you can do 8etter though!",
+                "8eat a 8lind like I taught you,",
+                "8ut keep 2 slots empty. Good luck!"
+            },
+            backseater_quest_3_done={
+                "AG: Now that I know you're not some half-thinkpan",
+                "8oy skylark, it's time for your first REAL challenge!"
+            },
+            backseater_quest_4_start={
+                "AG: Skip these 8a8y 8linds, go str8 for",
+                "the 8oss! I know you can do it! ::::)"
+            },
+            backseater_quest_4_done={
+                "AG: Hahahaha! I didn't actually think you'd",
+                "pull it off. I was fully ready to 8ounce.",
+                "Here, this next shop is on me."
+            },
+            backseater_quest_5_start={
+                "AG: I wasn't going to mention it 8efore, 8ut your",
+                "deck is preeeeeeeetty 8loated. I keep mine at",
+                "only 8 cards, 8ut that m8 8e a bit too much for you.",
+                "You should 8e a8le to take off at least 8 cards though!"
+            },
+            backseater_quest_5_done={
+                "AG: What? You're expecting a reward? That was",
+                "your reward, I made you thin out your misera8le",
+                "deck to make it easier to manipul8!"
+            },
+            backseater_quest_6_start={
+                "AG: If you want a reward, I've got another",
+                "challenge for you. Gener8 8 consuma8les in a",
+                "single 8lind. Shouldn't 8e too hard for a",
+                "poker prodigy like yourself :::;)"
+            },
+            backseater_quest_6_done={
+                "AG: Yeah!!!!!!!!! You're doing gr8, 8ut don't let that",
+                "get to your head! You've still got a looooooot to learn",
+                "from me 8efore I can give you my ultimate prize ::::)"
+            },
+            backseater_quest_7_start={
+                "AG: You've got one challenge left 8efore that though.",
+                "Show me what you're made of and trigger 20 cards",
+                "{E:2}in a single hand.{} Sounds impossi8le? It very",
+                "may well 8e for you. I won't tell you how to do it,",
+                "figure it out yourself! You need to learn to 8e independent!"
+            },
+            backseater_quest_7_done={
+                "Heyyyyyyyy. I've 8een going easy on you.",
+                "This next challenge is going to 8e unlike",
+                "anything you've faced so far. 8ut if you",
+                "can do it, my ultimate prize will 8e yours.",
+            },
+            backseater_quest_8_start={
+                "Score 16777216 chips in a single hand. If it sounds",
+                "like a lot, it's 8ecause it is. That's my current",
+                "highscore. 8eat it, and the reward will 8e yours.",
+                "8ut I understand if you're not cut out for it.",
+                "I'll 8e waiting."
+            },
+            backseater_quest_8_done={
+                "AG: Anyway, my time's running out and I gotta 8ounce.",
+                "For your trou8le I would like to pass my favorite jokers",
+                "on to you. If my outrageously gr8 luck has any say in the",
+                "matter, we will 8e meeting up in no time!L8r! <33333333"
+            },
+            backseater_invalid_tier={
+                "Something meddled with my varia8les",
+                "and I'm on an invalid quest num8er.",
+                "Soooooooo, wanna make out?"
+            },
         },
         zodiac_names = {
             Taurus = 'Taurus',
