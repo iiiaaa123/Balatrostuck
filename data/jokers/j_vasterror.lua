bd53ea6401533ba329819dd77254f7252290b7a6
EuSouAFazer <84548101+EuSouAFazer@users.noreply.github.com>
2025-11-01 21:43:12 -0300

    12 by 12 buff

--- data/jokers/j_vasterror.lua
+++ data/jokers/j_vasterror.lua
@@ -4,7 +4,7 @@ function Balatrostuck.INIT.Jokers.j_vasterror()
         key = "vasterror",
         config = {
             extra = {
-                rolls_needed = 7,
+                rolls_needed = 6,
                 rolls = 0,
                 valid_cards = {}
             }
@@ -13,8 +13,8 @@ function Balatrostuck.INIT.Jokers.j_vasterror()
             ['name'] = 'Twelve by Twelve',
             ['text'] = {
                 [1] = "Every {C:green}#1# rerolls{} in the shop, add",
-                [2] = "{C:paradox}Paradox{} edition and a {C:blue}Blue Seal",
-                [3] = "to a {C:attention}random{} playing card in {C:attention}deck",
+                [2] = "{C:paradox}Paradox{} edition and a {C:blue}Blue Seal{} to",
+                [3] = "two {C:attention}random{} playing cards in {C:attention}deck",
                 [4] = "{C:inactive}(Currently {C:attention}#2#{C:inactive}/#1# rerolls)"
             }
         },
@@ -40,7 +40,25 @@ function Balatrostuck.INIT.Jokers.j_vasterror()
             if context.reroll_shop and not context.blueprint then
                 card.ability.extra.rolls = card.ability.extra.rolls + 1
                 if card.ability.extra.rolls >= card.ability.extra.rolls_needed then
+                    -- First hit
+                    card.ability.extra.valid_cards = {}
+                    for k, v in pairs(G.deck.cards) do
+                        if not v.edition then
+                            card.ability.extra.valid_cards[#card.ability.extra.valid_cards+1] = v
+                        end
+                    end
 
+                    if card.ability.extra.valid_cards[1] then
+                        card_eval_status_text(card, 'extra', nil, nil, nil, {message = 'Paradox-ify!',colour = G.C.GREEN, card = card})
+                        card.ability.extra.rolls = 0
+                        local _card = pseudorandom_element(card.ability.extra.valid_cards,pseudoseed("arcjetted"))
+                        _card:set_edition("e_bstuck_paradox",true)
+                        _card:set_seal("Blue")
+                    else
+                        card_eval_status_text(card, 'extra', nil, nil, nil, {message = 'Miss!',colour = G.C.JOKER_GREY, card = card})
+                        card.ability.extra.rolls = 0
+                    end
+                -- Second hit
                     card.ability.extra.valid_cards = {}
                     for k, v in pairs(G.deck.cards) do
                         if not v.edition then
