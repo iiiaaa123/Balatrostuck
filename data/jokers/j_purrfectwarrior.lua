81291a56a4dad3d0341bee6f0f417b9a5d309cd0
CampfireCollective <122759859+GuilloryCraft@users.noreply.github.com>
2025-11-06 15:13:27 -0700

    for fazzie to test thank youuu
    feel  free to revert if its Fucked

--- data/jokers/j_purrfectwarrior.lua
+++ data/jokers/j_purrfectwarrior.lua
@@ -36,9 +36,9 @@ function Balatrostuck.INIT.Jokers.j_purrfectwarrior()
 
         loc_vars = function(self, info_queue, card)
             art_credit('baqly', info_queue)
-            return {vars = {card.ability.extra.h_gain, card.ability.extra.levels, card.ability.extra.h_plays}}
+            local thunk = math.floor(math.max((sum_levels()-12)/5,0))
+            return {vars = {card.ability.extra.h_gain, card.ability.extra.levels, thunk}}
         end,
-
 
         add_to_deck = function(self, card, from_debuff)
             card.ability.extra.h_plays = math.floor(math.max((sum_levels()-12)/5,0))
@@ -46,20 +46,20 @@ function Balatrostuck.INIT.Jokers.j_purrfectwarrior()
                 ease_hands_played(card.ability.extra.h_plays)
             end
             G.GAME.round_resets.hands = G.GAME.round_resets.hands + card.ability.extra.h_plays
-        end,        
+        end,
     
         remove_from_deck = function(self, card, from_debuff)
             G.GAME.round_resets.hands = G.GAME.round_resets.hands - card.ability.extra.h_plays
         end,
 
         calculate = function(self, card, context)
-            if context.bstuck_level_up_hand and not context.blueprint then
-                print("mreow B33")
-                if math.floor(math.max((sum_levels()-12)/5,0)) > card.ability.extra.h_plays then
-                    local thunk = math.floor(math.max((sum_levels()-12)/5,0)) - card.ability.extra.h_plays
+            if context.level_up_hand and not context.blueprint then
+                local thunk = math.floor(math.max((sum_levels()-12)/5,0))
+                if thunk > card.ability.extra.h_plays then
+                    thunk = thunk - card.ability.extra.h_plays
                     G.GAME.round_resets.hands = G.GAME.round_resets.hands + thunk
+                    card.ability.extra.h_plays = card.ability.extra.h_plays + thunk
                     ease_hands_played(thunk)
-                    card.ability.extra.h_plays = math.floor(math.max((sum_levels()-12)/5,0))
                 end
             end
         end,
@@ -69,6 +69,4 @@ function Balatrostuck.INIT.Jokers.j_purrfectwarrior()
             end
         end
     }
-    
-    
 end
