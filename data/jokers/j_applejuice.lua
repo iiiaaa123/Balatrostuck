85a90fae6bac60fbaa1d62f82b055e9dd82f605d
CampfireCollective <122759859+GuilloryCraft@users.noreply.github.com>
2025-12-03 15:15:00 -0700

    works with burglar/water

--- data/jokers/j_applejuice.lua
+++ data/jokers/j_applejuice.lua
@@ -58,6 +58,34 @@ function Balatrostuck.INIT.Jokers.j_applejuice()
                     return true end }))
                     end
                 end
+            elseif context.first_hand_drawn and G.GAME.current_round.discards_left == 0 and not context.blueprint then
+                local left, me = nil, nil
+                for k, v in ipairs(G.jokers.cards) do
+                    if v == card then me = v end
+                    if left == nil and v.config.center_key == 'j_bstuck_applejuice' then left = v end
+                end
+                if me == left then
+                    if card.ability.extra.d_size <= 1 then
+                        ease_discard(1, nil, true)
+                        card.getting_sliced = true
+                        G.E_MANAGER:add_event(Event({
+                            func = function()
+                                card:start_dissolve({G.C.RED}, nil, 1.6)
+                                return true;
+                            end
+                        }))
+                        return {
+                            message = localize('k_drank_ex'),
+                            colour = G.C.FILTER
+                        }
+                    else
+                    G.E_MANAGER:add_event(Event({func = function()
+                        ease_discard(1, nil, true)
+                        card.ability.extra.d_size = card.ability.extra.d_size - 1
+                        card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = "-1 Discard", colour = G.C.RED})
+                    return true end }))
+                    end
+                end
             end
         end
     }
