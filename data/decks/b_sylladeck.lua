ee71c03d160872f6db4b6f53b3e3d30db9540122
CampfireCollective <122759859+GuilloryCraft@users.noreply.github.com>
2025-12-09 00:43:50 -0700

    sylladeck lets gooo

---
+++ data/decks/b_sylladeck.lua
@@ -0,0 +1,22 @@
+function Balatrostuck.INIT.Decks.b_sylladeck()
+    SMODS.Back {
+        key = 'sylladeck',
+        loc_txt = {
+            name = "Sylladeck",
+            text = {
+                '{C:attention}+3{} Consumable Slots',
+                'All {C:attention}Jokers{} are {C:attention}Pinned'
+            }
+        },
+        config = {        },
+        loc_vars = function(self, info_queue, center)
+            return {vars = {}}
+        end,
+        atlas = 'HomestuckDecks',
+        pos = {x = 0,y = 0},
+        unlocked = true,
+        apply = function(self,back)
+            G.GAME.starting_params.consumable_slots = G.GAME.starting_params.consumable_slots + 3
+        end,
+    }
+end
