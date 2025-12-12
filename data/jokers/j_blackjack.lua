895d07cf24a50c9144de87a590ae54e151e541c4
alchemis <hopefuldecay@gmail.com>
2025-12-12 14:35:09 -0300

    implements blackjack in balatro (this will be balatro in 2016)

--- data/jokers/j_blackjack.lua
+++ data/jokers/j_blackjack.lua
@@ -3,14 +3,17 @@ function Balatrostuck.INIT.Jokers.j_blackjack()
         name = "Rules Card for Blackjack",
         key = "blackjack",
         config = {
-            extra = { 
+            extra = { activated = false
             }
         },
         loc_txt = {
             ['name'] = "Rules Card for Blackjack",
             ['text'] = {
-                [1] = "When round begins, create {C:green}#1#",
-                [2] = "{C:attention}Lucky {C:paradox}Paradox {C:clubs}4 of Clubs{} in hand"
+                "If the {C:attention}sum{} of the played",
+                "cards' {C:attention}ranks{} is 21",
+                "this gives +{C:mult}21{} mult",
+                "and all played cards {C:attention}score{}",
+                "{C:inactive} (Face cards are 10, Aces are 1 or 11)"
             }
         },
         pos = {
@@ -28,7 +31,37 @@ function Balatrostuck.INIT.Jokers.j_blackjack()
 
         atlas = 'HomestuckJokers',
         calculate = function (self, card, context)
-        
+            if context.modify_scoring_hand then
+                local _total_chips = 0
+                local _ace_count = 0
+                if #context.full_hand == 0 then return end
+                for _, played_card in ipairs(context.full_hand) do
+                    _total_chips = _total_chips + played_card.base.nominal
+                    if played_card.base.nominal == 11 then _ace_count = _ace_count + 1 end
+                end
+                while _ace_count > 0 do
+                        if _total_chips == 21 then break end
+                        _total_chips = _total_chips - 10
+                        _ace_count = _ace_count -1
+                end
+
+                
+                if _total_chips == 21 then 
+                    card.ability.extra.activated = true
+                    return { add_to_hand = true }
+
+                end
+            end
+            if context.joker_main then 
+                if card.ability.extra.activated then 
+                    card.ability.extra.activated = false
+                    return {
+                        mult_mod = 21,
+                        message = localize { type = 'variable', key = 'a_mult', vars = { 21 } },
+                        colour = G.C.MULT
+                    } 
+                end
+            end
         end,
         loc_vars = function (self, info_queue, card)
             art_credit('akai', info_queue)
