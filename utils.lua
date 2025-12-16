9195384d76174ec7cdaf6b95d97d114d54a3adf4
alchemis <hopefuldecay@gmail.com>
2025-12-16 15:51:54 -0300

    fuck you

--- utils.lua
+++ utils.lua
@@ -883,7 +883,10 @@ function add_tag(_tag,fromMind)
                 --next line is the "2x" text or whatever
                 --if a sprite needs to be added, it probably goes in here
                 {n=G.UIT.O, config={object = DynaText({string = {{suffix = "x", ref_table = _tag.ability.extra, ref_value = 'stack_count'}}, colours = {G.C.UI.TEXT_LIGHT},shadow = true, scale = 0.25})}},
-                UIBox_button({button = "delete_tag",label={"x"},minw=0.35,minh=0.35, ref_table = _tag,r=0.5,align="cl"})
+                --if you want to make it so that the main sprite is clickable without this extra ugly as sin button
+                --good luck! total hours spent trying: 5
+                --increase the counter after you give up :)
+                UIBox_button({colour = G.C.RED, button = "delete_tag",label={"x"},minw=0.30,minh=0.30, ref_table = _tag,r=0.5,align="cl"})
             }},
             config = {
                 align = G.HUD_tags[1] and 'tm' or 'bri',
