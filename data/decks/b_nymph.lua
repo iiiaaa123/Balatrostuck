f504f353385581facb68c1a43aa02d8c38f4137e
alchemis <hopefuldecay@gmail.com>
2025-12-21 20:21:26 -0300

    added some resets to nymph that it was missing, now resets shop packs for example

--- data/decks/b_nymph.lua
+++ data/decks/b_nymph.lua
@@ -24,6 +24,27 @@ function Balatrostuck.INIT.Decks.b_nymph()
                             G.blind_prompt_box:remove()
                             G.blind_select = nil
                         end
+                        G.GAME.current_round.discards_left = math.max(0, G.GAME.round_resets.discards + G.GAME.round_bonus.discards)
+                        G.GAME.current_round.hands_left = (math.max(1, G.GAME.round_resets.hands + G.GAME.round_bonus.next_hands))
+                        G.GAME.current_round.hands_played = 0
+                        G.GAME.current_round.discards_used = 0
+                        G.GAME.current_round.any_hand_drawn = nil
+                        G.GAME.current_round.reroll_cost_increase = 0
+                        G.GAME.current_round.used_packs = {}
+
+                        for k, v in pairs(G.GAME.hands) do 
+                            v.played_this_round = 0
+                        end
+
+                        for k, v in pairs(G.playing_cards) do
+                            v.ability.wheel_flipped = nil
+                        end
+
+                        G.GAME.current_round.free_rerolls = G.GAME.round_resets.free_rerolls
+                        calculate_reroll_cost(true)
+
+                        G.GAME.round_bonus.next_hands = 0
+                        G.GAME.round_bonus.discards = 0
                         G.GAME.current_round.jokers_purchased = 0
                         G.GAME.current_round.discards_left = math.max(0, G.GAME.round_resets.discards + G.GAME.round_bonus.discards)
                         G.GAME.current_round.hands_left = (math.max(1, G.GAME.round_resets.hands + G.GAME.round_bonus.next_hands))
@@ -31,8 +52,9 @@ function Balatrostuck.INIT.Decks.b_nymph()
                         G.GAME.shop_free = nil
                         G.GAME.shop_d6ed = nil
                         G.STATE_COMPLETE = false
+                        if G.STAGE == G.STAGES.RUN then  ease_round(1) end
                         return true
-                      end
+                    end
         }))
             end
         end
