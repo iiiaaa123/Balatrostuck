34d863b966c2d234b2b79811b5816fa6d60c21c2
alchemis <hopefuldecay@gmail.com>
2025-12-22 15:23:26 -0300

    fixed nymph deck

--- data/decks/b_nymph.lua
+++ data/decks/b_nymph.lua
@@ -11,28 +11,55 @@ function Balatrostuck.INIT.Decks.b_nymph()
         pos = {x = 0,y = 1},
         unlocked = true,
         calculate = function(self,back,context)
-            if context.skip_blind then
+            if context.skip_blind_post then
                 G.E_MANAGER:add_event(Event({
-                      trigger = 'immediate',
+                      trigger = 'after',
+                      delay = 2.5,
+                      blocking = false,
                       func = function()
-                        if G.round_eval then 
-                            G.round_eval:remove()
-                            G.round_eval = nil
+                        if (G.STATE ~= G.STATES.SMODS_BOOSTER_OPENED) then
+                            if G.round_eval then 
+                                G.round_eval:remove()
+                                G.round_eval = nil
+                            end
+                            if G.blind_select then
+                                G.blind_select:remove()
+                                G.blind_prompt_box:remove()
+                                G.blind_select = nil
+                            end
+                            G.GAME.current_round.discards_left = math.max(0, G.GAME.round_resets.discards + G.GAME.round_bonus.discards)
+                            G.GAME.current_round.hands_left = (math.max(1, G.GAME.round_resets.hands + G.GAME.round_bonus.next_hands))
+                            G.GAME.current_round.hands_played = 0
+                            G.GAME.current_round.discards_used = 0
+                            G.GAME.current_round.any_hand_drawn = nil
+                            G.GAME.current_round.reroll_cost_increase = 0
+                            G.GAME.current_round.used_packs = {}
+
+                            for k, v in pairs(G.GAME.hands) do 
+                                v.played_this_round = 0
+                            end
+
+                            for k, v in pairs(G.playing_cards) do
+                                v.ability.wheel_flipped = nil
+                            end
+
+                            G.GAME.current_round.free_rerolls = G.GAME.round_resets.free_rerolls
+                            calculate_reroll_cost(true)
+
+                            G.GAME.round_bonus.next_hands = 0
+                            G.GAME.round_bonus.discards = 0
+                            G.GAME.current_round.jokers_purchased = 0
+                            G.GAME.current_round.discards_left = math.max(0, G.GAME.round_resets.discards + G.GAME.round_bonus.discards)
+                            G.GAME.current_round.hands_left = (math.max(1, G.GAME.round_resets.hands + G.GAME.round_bonus.next_hands))
+                            G.STATE = G.STATES.SHOP
+                            G.GAME.shop_free = nil
+                            G.GAME.shop_d6ed = nil
+                            G.STATE_COMPLETE = false
+                            if G.STAGE == G.STAGES.RUN then  ease_round(1) end
+                            return true
                         end
-                        if G.blind_select then
-                            G.blind_select:remove()
-                            G.blind_prompt_box:remove()
-                            G.blind_select = nil
-                        end
-                        G.GAME.current_round.jokers_purchased = 0
-                        G.GAME.current_round.discards_left = math.max(0, G.GAME.round_resets.discards + G.GAME.round_bonus.discards)
-                        G.GAME.current_round.hands_left = (math.max(1, G.GAME.round_resets.hands + G.GAME.round_bonus.next_hands))
-                        G.STATE = G.STATES.SHOP
-                        G.GAME.shop_free = nil
-                        G.GAME.shop_d6ed = nil
-                        G.STATE_COMPLETE = false
-                        return true
-                      end
+                        return false
+                    end
         }))
             end
         end
