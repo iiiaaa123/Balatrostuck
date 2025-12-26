b182d95a1ddc0c4795247dd07fb78056f19ff4e4
alchemis <hopefuldecay@gmail.com>
2025-12-25 21:53:50 -0300

    jail key goes both ways now, quarters implemented

---
+++ data/jokers/j_quarters.lua
@@ -0,0 +1,61 @@
+function Balatrostuck.INIT.Jokers.j_quarters()
+    SMODS.Joker{
+        name = "Quarters",
+        key = "quarters",
+        config = {
+            extra = { 
+            }
+        },
+        loc_txt = {
+            ['name'] = 'Quarters',
+            ['text'] = { --Create a negative Jail Key when obtained. Jail Key creates negative jokers and is not consumed on use. (You can use it on jokers to transform a card back.) 
+
+
+                "Create a negative {C:attention}Jail Key{} when obtained.",
+                "{C:attention}Jail Key{} creates negative jokers",
+                "and is not consumed on use.",
+                "{C:inactive}(You can use it on jokers to transform a card back.)"
+            }
+        },
+        pos = {
+            x = 9,
+            y = 12
+        },
+
+        
+        cost = 4,
+        rarity = "bstuck_felt",
+        blueprint_compat = false,
+        eternal_compat = true,
+        unlocked = true,
+
+
+        atlas = 'HomestuckJokers',
+        calculate = function (self, card, context)
+            if context.using_consumeable == true and context.consumeable.config.center.key == 'c_bstuck_jailkey' and context.consumeable.edition and context.consumeable.edition.key == "e_negative" and not context.blueprint then
+                G.E_MANAGER:add_event(Event({func = function()
+                    local _card = SMODS.add_card({set = 'Spectral', key = 'c_bstuck_jailkey'})
+                    _card:set_edition("e_negative", true,true)
+                    return true
+                end }))
+            end
+            if context.check_eternal and context.trigger.from_sell == true and context.other_card.edition and context.other_card.edition.key == "e_negative" and context.other_card.config.center.key == 'c_bstuck_jailkey' then
+                return {no_destroy = {override_compat = true}}
+            end
+
+        end,
+        loc_vars = function (self, info_queue, card)
+            art_credit('akai', info_queue)
+            return {vars = {}}
+        end,
+        add_to_deck = function(self,card,from_debuff)
+            if not from_debuff then
+                G.E_MANAGER:add_event(Event({func = function()
+                    SMODS.add_card({set = 'Spectral', key = 'c_bstuck_jailkey', edition='e_negative'})
+
+                    return true
+                end }))
+            end
+        end
+    }
+end 
--- data/spectrals/c_spectral_jailkey.lua
+++ data/spectrals/c_spectral_jailkey.lua
@@ -21,7 +21,7 @@ function Balatrostuck.INIT.Spectrals.c_spectral_jailkey()
             local doublekey = nil
             local doublename = nil
             local colour = G.C.GREY
-            if G.hand and #G.hand.highlighted == 1 then
+            if G.hand and G.jokers and #G.hand.highlighted == 1 and #G.jokers.highlighted == 0 then
                 doublename = get_innocuous(G.hand.highlighted[1],true)
                 doublekey = get_innocuous(G.hand.highlighted[1],false)
                 local hehe = G.hand.highlighted[1]
@@ -38,14 +38,29 @@ function Balatrostuck.INIT.Spectrals.c_spectral_jailkey()
                 elseif hehe.base.suit == 'Spades' then
                     colour = G.C.SUITS.Spades 
                 end
+            elseif G.hand and G.jokers and #G.hand.highlighted == 0 and #G.jokers.highlighted == 1 then
+                doublename = get_innocuous(G.jokers.highlighted[1],true)
+                local doublekey, double_enh = get_innocuous(G.jokers.highlighted[1],false)
+                if doublekey then
+                    if double_enh and double_enh == "m_stone" then
+                        colour = G.C.JOKER_GREY
+                    elseif double_enh and double_enh == "m_wild" then
+                        colour = coolstuff
+                    elseif string.sub(doublekey, 1, 1) == 'H' then
+                        colour = G.C.SUITS.Hearts
+                    elseif string.sub(doublekey, 1, 1) == 'D'  then
+                        colour = G.C.SUITS.Diamonds 
+                    elseif string.sub(doublekey, 1, 1) == 'C'  then
+                        colour = G.C.SUITS.Clubs 
+                    elseif string.sub(doublekey, 1, 1) == 'S' then
+                        colour = G.C.SUITS.Spades 
+                    end
+                end
             else
                 doublename = 'Unselected'
-                local colour = G.C.GREY
+                colour = G.C.GREY
             end
 
-            if doublekey then
-                info_queue[#info_queue+1] = doublekey and G.P_CENTERS[doublekey] or nil
-            end
 
             if doublekey == 'j_bstuck_jocker' then
                 colour = G.C.RARITY[4]
@@ -72,35 +87,67 @@ function Balatrostuck.INIT.Spectrals.c_spectral_jailkey()
         },
         atlas = 'HomestuckSpectrals',
         can_use = function(self,card)
-            return #G.hand.highlighted == 1 and #G.jokers.cards < G.jokers.config.card_limit
+            return (#G.hand.highlighted == 1 and #G.jokers.cards < G.jokers.config.card_limit and #G.jokers.highlighted == 0) 
+            or ((#G.hand.cards > 0 and #G.hand.highlighted == 0 and #G.jokers.highlighted == 1) and get_innocuous(G.jokers.highlighted[1],false))
         end,
         use = function(self, card, area, copier)
-            local _card = G.hand.highlighted[1]
-            G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.4, func = function()
-                play_sound('tarot1')
-                card:juice_up(0.3, 0.5)
-                return true end }))
+            if #G.hand.highlighted ~= 0 then
+                local _card = G.hand.highlighted[1]
+                G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.4, func = function()
+                    play_sound('tarot1')
+                    card:juice_up(0.3, 0.5)
+                    return true end }))
+                    G.E_MANAGER:add_event(Event({
+                        func = function()
+                            play_sound('timpani')
+                            local new_card = SMODS.create_card({set = 'Joker',no_edition = true, key = get_innocuous(_card,false)})
+                            new_card:start_materialize(nil, false)
+                            G.jokers:emplace(new_card)
+                            if next(SMODS.find_card('j_bstuck_quarters')) then
+                                new_card:set_edition('e_negative')
+                            end
+                            return true
+                        end
+                    }))
+                    G.E_MANAGER:add_event(Event({
+                    trigger = 'after',
+                    delay = 0.2,
+                    func = function() 
+                        if _card.ability.name == 'Glass Card' then 
+                            _card:shatter()
+                        else
+                            _card:start_dissolve(nil, i == #G.hand.highlighted)
+                        end
+                        return true 
+                end }))
+                delay(0.2)
+            else
+                local _card = G.jokers.highlighted[1]
+                G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.4, func = function()
+                    play_sound('tarot1')
+                    card:juice_up(0.3, 0.5)
+                    return true end }))
                 G.E_MANAGER:add_event(Event({
-                    func = function()
-                        play_sound('timpani')
-                        local new_card = SMODS.create_card({set = 'Joker',no_edition = true, key = get_innocuous(_card,false)})
-                        new_card:start_materialize(nil, false)
-                        G.jokers:emplace(new_card)
-                        return true
+                func = function()
+                    play_sound('timpani')
+                    local card_key, card_enh = get_innocuous(_card,false)
+                    local new_card = create_playing_card({front = G.P_CARDS[card_key], center = G.P_CENTERS.c_base}, nil, nil, nil, {G.C.SECONDARY_SET.Enhanced})
+                    if card_enh then 
+                        new_card:set_ability(G.P_CENTERS[card_enh])
                     end
-                }))
+                    --new_card:start_materialize(nil, false)
+                    G.hand:emplace(new_card)
+                    return true
+                end}))
                 G.E_MANAGER:add_event(Event({
                 trigger = 'after',
                 delay = 0.2,
                 func = function() 
-                    if _card.ability.name == 'Glass Card' then 
-                        _card:shatter()
-                    else
-                        _card:start_dissolve(nil, i == #G.hand.highlighted)
-                    end
+                    _card:start_dissolve(nil, i == #G.jokers.highlighted)
                     return true 
-            end }))
-            delay(0.2)
+                end }))
+                delay(0.2)
+            end
         end
     }
 end
