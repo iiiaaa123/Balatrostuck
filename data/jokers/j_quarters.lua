a5b3e8a18a99efbcff5651370a46d0f8f4b42df0
alchemis <hopefuldecay@gmail.com>
2025-12-28 02:43:38 -0300

    quarters fixes

--- data/jokers/j_quarters.lua
+++ data/jokers/j_quarters.lua
@@ -35,15 +35,19 @@ function Balatrostuck.INIT.Jokers.j_quarters()
 
         atlas = 'HomestuckJokers',
         calculate = function (self, card, context)
-            if context.using_consumeable == true and context.consumeable.config.center.key == 'c_bstuck_jailkey' and context.consumeable.edition and context.consumeable.edition.key == "e_negative" and not context.blueprint then
+            if context.using_consumeable == true and context.consumeable.config.center.key == 'c_bstuck_jailkey' 
+            and context.consumeable.edition and context.consumeable.edition.key == "e_negative" and context.consumeable.quarters_that_created_this == card
+            and not context.blueprint then
                 G.E_MANAGER:add_event(Event({func = function()
                     local _card = SMODS.add_card({set = 'Spectral', key = 'c_bstuck_jailkey'})
                     _card:set_edition("e_negative", true,true)
+                    _card.quarters_that_created_this = card
                     G.consumeables:add_to_highlighted(_card)
                     return true
                 end }))
             end
-            if context.check_eternal and context.trigger.from_sell == true and context.other_card.edition and context.other_card.edition.key == "e_negative" and context.other_card.config.center.key == 'c_bstuck_jailkey' then
+            if context.check_eternal and context.trigger.from_sell == true and context.other_card.quarters_that_created_this and context.other_card.edition
+            and context.other_card.edition.key == "e_negative" and context.other_card.config.center.key == 'c_bstuck_jailkey' then
                 return {no_destroy = {override_compat = true}}
             end
 
@@ -55,8 +59,8 @@ function Balatrostuck.INIT.Jokers.j_quarters()
         add_to_deck = function(self,card,from_debuff)
             if not from_debuff then
                 G.E_MANAGER:add_event(Event({func = function()
-                    SMODS.add_card({set = 'Spectral', key = 'c_bstuck_jailkey', edition='e_negative'})
-
+                    local _card = SMODS.add_card({set = 'Spectral', key = 'c_bstuck_jailkey', edition='e_negative'})
+                    _card.quarters_that_created_this = card
                     return true
                 end }))
             end
