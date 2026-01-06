c17fec01502ad9ff6e7478097c218eeb041db0dd
EuSouAFazer <84548101+EuSouAFazer@users.noreply.github.com>
2026-01-06 14:07:16 -0300

    rules card lil update

--- data/jokers/j_blackjack.lua
+++ data/jokers/j_blackjack.lua
@@ -1,19 +1,18 @@
 function Balatrostuck.INIT.Jokers.j_blackjack()
     SMODS.Joker{
-        name = "Rules Card for Blackjack",
+        name = "Blackjack Rules Card",
         key = "blackjack",
         config = {
             extra = { activated = false
             }
         },
         loc_txt = {
-            ['name'] = "Rules Card for Blackjack",
+            ['name'] = "Blackjack Rules Card",
             ['text'] = {
-                "If the {C:attention}sum{} of the played",
-                "cards' {C:attention}ranks{} is 21",
-                "this gives +{C:mult}21{} mult",
+                "+{C:mult}21{} mult if the {C:attention}sum{} of the",
+                "played cards' {C:attention}ranks{} is 21",
                 "and all played cards {C:attention}score{}",
-                "{C:inactive} (Face cards are 10, Aces are 1 or 11)"
+                "{C:inactive} (Faces are 10, Aces are 1 or 11)"
             }
         },
         pos = {
